68875c4fef88619c41d5b84bda16b02f100c3ebe
Raam <123749532+RaamHorakeri@users.noreply.github.com>
2024-10-31 13:00:12 +0530

    Update microservicePipeline.groovy

--- vars/microservicePipeline.groovy
+++ vars/microservicePipeline.groovy
@@ -1,4 +1,4 @@
-// microservicePipeline.groovy
+// vars/microservicePipeline.groovy
 
 def call(String branch, String gitUrl, String imageName, String containerName, String portMapping) {
     pipeline {
@@ -6,25 +6,36 @@ def call(String branch, String gitUrl, String imageName, String containerName, S
         stages {
             stage('Checkout from Git') {
                 steps {
-                    git branch: branch, url: gitUrl
+                    script {
+                        env.GIT_BRANCH = branch  // Set the branch as an environment variable
+                        env.GIT_URL = gitUrl     // Set the Git URL as an environment variable
+                        git branch: env.GIT_BRANCH, url: env.GIT_URL
+                    }
                 }
             }
             stage("Docker Build") {
                 steps {
                     script {
-                        sh "docker build -t ${imageName}:latest ."
+                        env.IMAGE_NAME = imageName  // Set the image name as an environment variable
+                        sh "docker build -t ${env.IMAGE_NAME}:latest ."
                     }
                 }
             }
             stage("Remove container") {
                 steps {
-                    sh "docker stop ${containerName} || true"
-                    sh "docker rm ${containerName} || true"
+                    script {
+                        env.CONTAINER_NAME = containerName  // Set the container name as an environment variable
+                        sh "docker stop ${env.CONTAINER_NAME} || true"
+                        sh "docker rm ${env.CONTAINER_NAME} || true"
+                    }
                 }
             }
             stage('Deploy to container') {
                 steps {
-                    sh "docker run -d --name ${containerName} -p ${portMapping} ${imageName}:latest"
+                    script {
+                        env.PORT_MAPPING = portMapping  // Set the port mapping as an environment variable
+                        sh "docker run -d --name ${env.CONTAINER_NAME} -p ${env.PORT_MAPPING} ${env.IMAGE_NAME}:latest"
+                    }
                 }
             }
         }
