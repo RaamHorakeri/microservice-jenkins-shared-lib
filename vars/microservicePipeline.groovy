a2f37a6fbdfc8b57a3443f81e947308b7938e223
Raam <123749532+RaamHorakeri@users.noreply.github.com>
2024-10-31 13:02:44 +0530

    Update microservicePipeline.groovy

--- vars/microservicePipeline.groovy
+++ vars/microservicePipeline.groovy
@@ -7,34 +7,29 @@ def call(String branch, String gitUrl, String imageName, String containerName, S
             stage('Checkout from Git') {
                 steps {
                     script {
-                        env.GIT_BRANCH = branch  // Set the branch as an environment variable
-                        env.GIT_URL = gitUrl     // Set the Git URL as an environment variable
-                        git branch: env.GIT_BRANCH, url: env.GIT_URL
+                        git branch: branch, url: gitUrl
                     }
                 }
             }
             stage("Docker Build") {
                 steps {
                     script {
-                        env.IMAGE_NAME = imageName  // Set the image name as an environment variable
-                        sh "docker build -t ${env.IMAGE_NAME}:latest ."
+                        sh "docker build -t ${imageName}:latest ."
                     }
                 }
             }
             stage("Remove container") {
                 steps {
                     script {
-                        env.CONTAINER_NAME = containerName  // Set the container name as an environment variable
-                        sh "docker stop ${env.CONTAINER_NAME} || true"
-                        sh "docker rm ${env.CONTAINER_NAME} || true"
+                        sh "docker stop ${containerName} || true"
+                        sh "docker rm ${containerName} || true"
                     }
                 }
             }
             stage('Deploy to container') {
                 steps {
                     script {
-                        env.PORT_MAPPING = portMapping  // Set the port mapping as an environment variable
-                        sh "docker run -d --name ${env.CONTAINER_NAME} -p ${env.PORT_MAPPING} ${env.IMAGE_NAME}:latest"
+                        sh "docker run -d --name ${containerName} -p ${portMapping} ${imageName}:latest"
                     }
                 }
             }
